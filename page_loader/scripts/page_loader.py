6fe19f56b18076e2a67a1118898e916b531f2d55
alexander_potapkov <xanben@mail.ru>
2022-10-24 17:38:46 +0300

    add loader.py page_loader.py test_page_loader.py

---
+++ page_loader/scripts/page_loader.py
@@ -0,0 +1,17 @@
+import argparse
+import os
+import pathlib
+from page_loader.loader import download
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Page loader',
+                                     conflict_handler='resolve')
+    parser.add_argument('url')
+    parser.add_argument('path', type=pathlib.Path, default=os.getcwd())
+    args = parser.parse_args()
+    download(args.url, args.path)
+
+
+if __name__ == '__main__':
+    main()
