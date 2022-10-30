da6b4fbc08de12ef9aa5932703b041369a811e35
alexander_potapkov <xanben@mail.ru>
2022-10-31 00:46:26 +0300

    make func get_images

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -1,26 +1,39 @@
 import os
-import pathlib
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-from page_loader.namer import get_dir_name, get_file_name
+from page_loader.namer import get_dir_name, get_file_name, get_img_name
 
 
-def is_valid_url(url: str) -> bool:
+def is_valid_url(url):
     parsed = urlparse(url)
+
     return bool(parsed.netloc) and bool(parsed.scheme)
 
 
-# def get_images(url: str) -> list:
-#    html = requests.get(url).text
-#    soup = BeautifulSoup(html, "html.parser")
+def get_images(new_folder, soup):
+    if not os.path.exists(new_folder):
+        os.mkdir(new_folder)
+    for res in soup.findAll('img'):
+        filename = os.path.basename(res['src'])
+        filepath = os.path.join(new_folder, get_img_name(filename))
+        res['src'] = os.path.join(os.path.basename(new_folder), get_img_name(filename))
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.write(soup.prettify())
+            print(filepath)
+
+    return soup
 
 
-def download(url: str, output: str = pathlib.Path.cwd()) -> str:
-    save_file = requests.request('GET', url).text
-    with open(get_file_name(url, output), 'w', encoding='utf-8') as f:
-        if not os.path.isfile(save_file):
-            f.write(save_file)
-    if not os.path.isdir(get_dir_name(url, output)):
-        os.mkdir(get_dir_name(url, output))
+def download(url, output):
+    if is_valid_url(url):
+        response = requests.Session().get(url)
+        save_file = response.url
+        soup = BeautifulSoup(response.text, 'html.parser')
+        new_dir = get_dir_name(url, output)
+        soup = get_images(new_dir, soup)
+        with open(get_file_name(url, output), 'w', encoding='utf-8') as f:
+            if not os.path.isfile(soup.prettify()):
+                f.write(soup.prettify())
+
     return f'{get_dir_name(url, output)}\n' f'{get_file_name(url, output)}'
--- page_loader/namer.py
+++ page_loader/namer.py
@@ -1,17 +1,27 @@
 import re
+from urllib.parse import urlparse
+from pathlib import Path
+
+
+def remove_scheme(url):
+    parsed_url = urlparse(url)
+    return parsed_url.netloc + parsed_url.path
 
 
 def get_file_name(url, output):
-    if url.startswith('http:'):
-        filename = re.sub(r'\W', '-', url[7::])
-    elif url.startswith('https:'):
-        filename = re.sub(r'\W', '-', url[8::])
+    filename = re.sub(r'[^a-zA-Z0-9]', '-', remove_scheme(url))
+
     return f'{output}/{filename}.html'
 
 
 def get_dir_name(url, output):
-    if url.startswith('http:'):
-        filename = re.sub(r'\W', '-', url[7::])
-    elif url.startswith('https:'):
-        filename = re.sub(r'\W', '-', url[8::])
+    filename = re.sub(r'[^a-zA-Z0-9]', '-', remove_scheme(url))
+
     return f'{output}/{filename}_files'
+
+
+def get_img_name(filename):
+    path = Path(filename)
+    img_name = re.sub(r'[^a-zA-Z0-9]', '-', str(path.with_suffix('')))
+
+    return f'{img_name}.png'
